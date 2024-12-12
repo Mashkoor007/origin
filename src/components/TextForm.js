4d397cd75e3fe39ac632ad66e0fad26a2b7b371f
Mashkoor007 <mjcoding123@gmail.com>
2024-12-13 01:27:06 +0530

    CWH-YT8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,15 +19,23 @@ const handleOnchange = (event) => {
 //setText ("New Text by setText");
 //setText("dhjkgdfjhsgj");
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
       <div className="mb-3">
         <label htmlFor="myBox" className="form-label">Example textarea</label>
         <textarea className="form-control" id="myBox" rows="6" onChange={handleOnchange} value={text}></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert To Upper-Case</button> &nbsp;
-      <button className="btn btn-primary" onClick={handleLowClick}>Convert To Lower-Case</button>
-
+      <button className="btn btn-primary my-2" onClick={handleUpClick}>To Upper-Case</button> &nbsp;
+      <button className="btn btn-primary" onClick={handleLowClick}>To Lower-Case</button>
     </div>
+    <div className="container my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * (text.split(" ").length)} Minutes in read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   );
 }
